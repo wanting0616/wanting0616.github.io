305ee2b0e3e365e4d7698f3908317f2462c9aff4
joakim.liu <joakimliu28@gmail.com>
2025-07-25 15:01:52 +0800

    修改文件夹测试用

--- docs/.vuepress/themeConfig.js
+++ docs/.vuepress/themeConfig.js
@@ -6,49 +6,41 @@ import filehelper from './utils/initPage.js';
 export default {
         nav: [
           {
-            text: '面试',
-            link: '/Interview/',
-          },
-          {
             text: 'JavaScript',
-            link: '/JavaScript/',
+            link: '../JavaScript/',
           },
           {
             text: 'Vuejs',
-            link: '/vue/',
+            link: '../vue/',
           },
-          {
-            text: 'CSS',
-            link: '/css/',
-          },
-          {
-            text: '其它',
-            ariaLabel: 'Menu',
-            items: [
-              { text: 'VuePress', link: '/other/vuepress/' },
-              { text: '开发工具', link: '/other/ide/' }
-            ]
-          },
-          {
-            text: '主页',
-            ariaLabel: 'Menu',
-            items: [
-              { text: '主页一', link: '/404/' },
-              { text: '主页二', link: '/404/Personal' }
-            ]
-          },
+        //   {
+        //     text: '其它',
+        //     ariaLabel: 'Menu',
+        //     items: [
+        //       { text: 'VuePress', link: '/other/vuepress/' },
+        //       { text: '开发工具', link: '/other/ide/' }
+        //     ]
+        //   },
+        //   {
+        //     text: '主页',
+        //     ariaLabel: 'Menu',
+        //     items: [
+        //       { text: '主页一', link: '/404/' },
+        //       { text: '主页二', link: '/404/Personal' }
+        //     ]
+        //   },
         ],
         
-        sidebar: {
-          '/JavaScript/': concatJs(),
-        },
+        // sidebar: {
+        //   '../JavaScript/': concatJs(),
+        // },
   }
 
 // JavaScript
-function concatJs() {
-  const arr = utils.genSidebar( 'JStst', filehelper.getFileName(rootpath + '/JavaScript/'), false);
-  arr.push(...utils.genSidebar('Js-Vue',filehelper.getFileName(rootpath + '/JavaScript/vue/', 'vue/'),false));
-  return arr;
-}
+// function concatJs() {
+//   const arr = utils.genSidebar( 'JStst', filehelper.getFileName(rootpath + '/JavaScript/'), false);
+//   arr.push(...utils.genSidebar('Js-Vue',filehelper.getFileName(rootpath + '/JavaScript/vue/', 'vue/'),false));
+//   return arr;
+// }
 
 
