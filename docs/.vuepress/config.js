2ca2792b743a8d29f730648695c325bd011022fa
joakim.liu <joakimliu28@gmail.com>
2025-07-25 15:52:57 +0800

    修改config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,36 +1,25 @@
 import themeConfig from './themeConfig.js'
 
-module.exports = {
-    title: 'Hello VuePress', //网站的标题
-    description: 'Just playing around', //网站的描述
-    // themeConfig
+// module.exports = {
+//     title: 'Hello VuePress', //网站的标题
+//     description: 'Just playing around', //网站的描述
+//     themeConfig
+//   }
+
+  module.exports = {
+    title: '我的博客',
+    description: 'XXX',
     themeConfig: {
-        nav: [
-            {
-              text: 'JavaScript',
-              link: '/JavaScript/',
-            },
-            {
-              text: 'Vuejs',
-              link: '/vue/',
-            },
-          ],
-          sidebar: [
-              {
-                title: "欢迎学习",
-                path: "/",
-                collapsable: false,  // 是否折叠
-                children: [{ title: "博客简介", path: "/" }],
-              },
-              {
-                title: "基础篇",
-                path: "/Vue/1",
-                collapsable: true,
-                children: [
-                  { title: "第一篇", path: "/Vue/1" },
-                  { title: "第二篇", path: "/Vue/2" },
-                ]
-              } 
-            ]
-      }
+      nav: [
+        { text: "首页", link: "/" },
+        {
+          text: "codinglin 的博客",
+          items: [
+            { text: "掘金", link: "https://juejin.cn/user/726107228492253" },
+            { text: "Github", link: "https://github.com/coding-lin" }
+          ]
+        }
+      ]
+    }
   }
+  
